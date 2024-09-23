14624298eb539cc25cf48cf8c43fd0b75c920440
sammya <sammyacaroline90@gmail.com>
2024-09-22 21:07:39 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("oi")
+
+a = int(input("Digite o valor de a:  "))
+b = int(input("Digite o valor de b:  "))
+diferenca = a - b
+print(diferenca)
+if diferenca == 9:
+    print("O valor é igual a {}".format(diferenca))
+else:
+    print("O valor é diferente de 9.")
